0cf24de920203f3d022414499f1d2a865f063cfa
Marcey1611 <marcel.eichelberger@t-online.de>
2023-10-13 15:20:19 +0200

    App.vue unnötiges gelöscht und auskommentiert, main.js import von main.css auskommmentiert, AboutView.vue gelöscht, HomeView.vue angepasst

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+//import Scanner from '../components/Scanner.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!--<Scanner />-->
   </main>
 </template>
