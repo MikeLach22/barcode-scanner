ae598f6360bb8979587986c0531b8f5c4056d248
MikeLach22 <mikelachmuth@gmail.com>
2023-10-13 15:51:30 +0200

    Added Scanner Component

---
+++ src/components/Scanner.vue
@@ -0,0 +1,6 @@
+<script setup>
+    import { StreamBarcodeReader } from 'vue-barcode-reader';
+</script>
+<template>
+    <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+</template>
