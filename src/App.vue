f92544ed0b64a1e6f334f6307793a22c36465386
Marcey1611 <marcel.eichelberger@t-online.de>
2023-10-13 15:40:47 +0200

    created ListView.vue, modified App.vue, modified index.js

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <!--<RouterLink to="/about">About</RouterLink>-->
+        <RouterLink to="/list">List</RouterLink>
       </nav>
     </div>
   </header>
