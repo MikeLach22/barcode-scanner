6c37f8130ce42f3ae14215021efe1f9b4b14d8c1
np-223996 <np-223996@rwu.de>
2023-10-13 15:46:17 +0200

    Productcomponent

---
+++ src/components/Product.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- HTML-Template für die Komponente -->
+    <div>
+        <h1>{{ message }}</h1>
+        <img src="'/images/'+'objectName+/.jpeg" alt="objectName">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // JavaScript-Code für die Komponente
+    props: {
+            objectName: String,
+            description: String,
+            barCodeNumber: Number,
+        },
+    data() {
+        return{
+            objectName:"",
+            description: "",
+            barCodeNumber: 0,
+        };
+    },
+    methods: {
+      changeMessage() {
+        this.message = 'Nach Klick geändert!';
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  /* CSS-Stile für die Komponente */
+  </style>
